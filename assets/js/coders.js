8468657a25c8262aba577cd24748ecf0c1edc051
Luz Gutierrez <mili01gm@gmail.com>
2017-04-07 15:41:37 -0500

    más avance

---
+++ assets/js/coders.js
@@ -0,0 +1,22 @@
+var students=["Fiorella Quispe","Lourdes Vilchez","Rossmery Maldonado","Yessenia","Miriam Perralta","Elizabeth Condori","Arantza Burga","Grecia Rayme","Janine Vega","Rosario Felix","Daguiana Revoredo","Jenny Velasquez","Nadia Cuadros","Michell More","Marilu Llamocca","Mariel Garcia","Fiorella Cisneros","Geraldine Chauca","Yellitza Rivera","Stephanie Hiyagon","Emma Tapia","Danna Franco","Flor Retamozo","Nathaly Pacheco",
+"Ericka Vidal","Katherine Ortega","Brilly Majuan","Flor Tello","Leslie Avendaño","Cindy Mendoza","Annia Flores","Betsi Loayza","Aida Sulca","Milagros Gutierrez","Nakarid Jave","Angie Cóndor","Maricarmen Rojas","Ariana Cabana","Flor Condori","Mitch Rodríguez","Naomi Villanueva","Mari Castillo","Miriam Peralta","Karin Alejo","Liliana Peña","Ruth Salvador","Maribel","Wendy Reyes","Cinthia",
+"Maria Grecia Cutipa","Ana Durand","Glisse Jorge","Neiza Nuñez","Sandra Solorzano"];
+
+// <div>
+//   <figure class="coder">
+//   <img src="assets/img/students/4.png" alt="FiorellaQuispe">
+//   <figcaption>
+//     <span>Fiorella Quispe</span>
+//   </figcaption>
+// </figure>
+// </div>
+
+function crearCoder {
+  var div = document.createElement("div");
+  var contFoto = document.createElement("figure");
+  contFoto.setAttribute("class","coder");
+
+
+
+
+}
